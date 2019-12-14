b16e9b60e846067271949369ad1fed1588794124
Andrey Shatalov <a.shatalov@rj-games.com>
2019-12-15 02:00:16 +0300

    clean gh-pages branch

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/travis/build/rj-ashatalov/otuscpp-hw-05
-# Build directory: /home/travis/build/rj-ashatalov/otuscpp-hw-05
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(print_ip_tests "test_version")
